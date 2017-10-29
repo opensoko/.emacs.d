3c5fc2eeefeb53d3d0194ecf324b009832928180
Eugene Rwagasore <eugene.rwagasore@fao.org>
2017-10-29 04:08:55 +0200

    Added Shell, Magit and other packages configs

--- custom.el
+++ custom.el
@@ -3,7 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (use-package))))
+ '(package-selected-packages
+   (quote
+    (markdown-mode magit multiple-cursors smartparens smartscan zoom-frm scratch web-mode smex ido-vertical-mode exec-path-from-shell use-package)))
+ '(tramp-syntax (quote default) nil (tramp)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
